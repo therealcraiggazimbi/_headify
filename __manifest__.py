7c68878e0ca332223e0a7ddeb011eb9010294116
therealcraiggazimbi <craiggazimbi@gmail.com>
2024-05-01 13:02:25 +0200

    MIT LICENSE ADDED

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
         Headify aims to elevate the visual appeal and functionality of your Odoo website header. By implementing a comprehensive revamp, we focus on enhancing user experience through engaging image covers and an intuitive hamburger menu. With these enhancements, navigating your Odoo website becomes seamless and effortless for visitors. The new header design not only captivates attention with striking visuals but also ensures easy access to essential navigation options, facilitating smoother interaction and exploration of your website's content. By incorporating these features, our module aims to elevate the overall user experience, driving higher engagement and satisfaction levels among your website visitors.
     """,
     "author": "Craig Gazimbi",
-    "website": "https://github.com/therealcraiggazimbi/_python/tree/main/headify",
+    "website": "https://github.com/therealcraiggazimbi/_headify",
     "category": "Website",
     "version": "0.0.1",
     "depends": ["base", "headify"],
